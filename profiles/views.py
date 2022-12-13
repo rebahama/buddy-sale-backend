46440ecfeaca6046470262a897350a9f976b1feb
rebahama <wiri@hotmail.se>
2022-12-13 05:09:53 +0000

    add profile serializer

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from . models import Profile
+from . serializer import ProfileSeralizer
 
-# Create your views here.
+
+class ProfileList(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSeralizer
