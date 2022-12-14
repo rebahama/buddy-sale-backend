e87dcf267854775332a07693e19ef8ec7ec014b3
rebahama <wiri@hotmail.se>
2022-12-14 02:53:12 +0000

    add acoount ownership

--- profiles/serializer.py
+++ profiles/serializer.py
@@ -4,13 +4,13 @@ from . models import Profile
 
 class ProfileSeralizer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    
+    def get_is_owner(self, obj):
+        """ How to check for profile ownership"""
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
         fields = '__all__'
-
-
-def get_is_owner(self, obj):
-        """ How to check for profile ownership"""
-        request = self.context['request']
-        return request.user == obj.owner
