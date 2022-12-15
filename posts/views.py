a7a7a73bd2fb0b8cf08648ccf2892057831a00a5
rebahama <wiri@hotmail.se>
2022-12-15 03:10:30 +0000

    add post detalier

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Post
 from .serializer import PostSerializer
+from buddysale_drf.permissions import IsOwnerOrReadOnly
 
 
 class PostList(generics.ListCreateAPIView):
@@ -10,3 +11,9 @@ class PostList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class PostDetalier(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
