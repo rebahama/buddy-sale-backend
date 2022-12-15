179f3ac4f582fbe0d531dc0eca011817d87d1df0
rebahama <wiri@hotmail.se>
2022-12-15 03:57:06 +0000

    add city modal

---
+++ posts/citymodels.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class City (models.Model):
+    city = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.city}'
