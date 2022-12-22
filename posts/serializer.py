957951f502d2c55de298e653e60aa518e5096a2a
rebahama <wiri@hotmail.se>
2022-12-22 13:09:44 +0000

    add favorite_id

--- posts/serializer.py
+++ posts/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from . models import Post
+from favorites.models import Favorite
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -8,10 +9,21 @@ class PostSerializer(serializers.ModelSerializer):
     category_name = serializers.ReadOnlyField(source='category.title')
     city_name = serializers.ReadOnlyField(source='city.city')
     is_owner = serializers.SerializerMethodField()
+    favorite_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_favorite_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            favorite = Favorite.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return favorite.id if favorite else None
+        return None
+
 
     class Meta:
         model = Post
