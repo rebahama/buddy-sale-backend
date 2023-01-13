5f994a6cfc9b24d7ff9a1d95d045d6d2a6b96f09
rebahama <wiri@hotmail.se>
2023-01-13 04:40:04 +0000

    image validation

--- posts/serializer.py
+++ posts/serializer.py
@@ -36,9 +36,9 @@ class PostSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         uploaded_images = validated_data.pop("uploaded_images")
-        if len(uploaded_images) > 2:
-            uploaded_images = uploaded_images[:2]
-            self.errors['uploaded_images'] = ['You can upload only 4 images']
+        if len(uploaded_images) > 3:
+            uploaded_images = uploaded_images[:3]
+            self.errors['uploaded_images'] = ['You can upload only 3 images']
         post = Post.objects.create(**validated_data)
         for image in uploaded_images:
             new_image = MultipleImage.objects.create(post=post, image=image)
